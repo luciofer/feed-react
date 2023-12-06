4d6694c3e10e59f8e80aec748bbe67639ccc1464
Lucio Araujo <contatolucioaraujo@gmail.com>
2023-12-06 12:37:41 -0300

    Added sidebar component

--- README.md
+++ README.md
@@ -1,1 +1,18 @@
+# Ignite Feed
 
+Project in development, created as part of my ReactJs studies.
+
+
+## 👩🏾‍💻 Technologies
+
+- ReactJS
+- JavaScript and TypeScript
+
+
+
+
+## ✉ Contact
+
+contatolucioaraujo@gmail.com
+
+[Linkedin](https://www.linkedin.com/in/lucioaraujo30/)
--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,34 @@
 import './global.css'
+import styles from './App.module.css'
+
 import { Post } from './Post'
 import { Header } from './Components/Header'
+import { Sidebar } from './Components/Sidebar'
+
+
 
 export function App() {
   return (
     <>
     <Header />
+
+    <div className={styles.wrapper}>
+
+    <Sidebar />
+    
+    <main>
     <Post 
       author = "LF"
       content = "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Vitae, blanditiis molestias nostrum ipsam temporibus commodi consequatur. Illo quibusdam ipsa atque itaque vitae. Sint accusamus eum non. Ducimus sint exercitationem ipsum."
     />
-
     <Post 
       author = "LF 2"
       content = "Lorem impsum 2"
     />
+    </main>
+
+    </div>
+
     </>
   )
 }
