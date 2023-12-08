dd8ceb12bcc01e3bc73a90a68b5eb30e74b8bfb7
Lucio Araujo <contatolucioaraujo@gmail.com>
2023-12-08 17:56:34 -0300

    Implemented dynamic data into the post

--- src/App.jsx
+++ src/App.jsx
@@ -6,6 +6,40 @@ import { Header } from './Components/Header'
 import { Sidebar } from './Components/Sidebar'
 import { Post } from './Components/Post'
 
+// author = {avatar_url: "", name: "", role: ""}
+//publishedAt = Date
+// content = String
+
+const posts = [
+    {
+      id: 1,
+      author: {
+        avatarUrl:"https://media.licdn.com/dms/image/D4D03AQHpQ13Iz9Ps0w/profile-displayphoto-shrink_800_800/0/1692132894255?e=1707350400&v=beta&t=mzgF0v59NcabLRk_aYK0-s9qhtB6GVpbfwSuIFaAljw",
+        name: "Lucio Araujo",
+        role: "Software Developer"
+      },
+      content: [
+        {type: 'paragraph', content: 'Hey there 👋'},
+        {type: 'paragraph', content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate molestias sint numquam quidem, corporis, ipsa magni ducimus vel pariatur officia architecto maiores nostrum voluptas nisi non voluptates animi consectetur adipisci. 🚀'},
+        {type: 'link', content: 'lf.dev/newproject'},
+      ],
+      publishedAt: new Date('2023-12-07 17:00:00')
+    },
+    {
+      id: 2,
+      author: {
+        avatarUrl:"https://media.licdn.com/dms/image/D4D03AQFUIaMMNRQG7Q/profile-displayphoto-shrink_800_800/0/1686611375662?e=1707350400&v=beta&t=EYdp8K0HfBGLqEKPVM46M9Wggj57QMAmTYAzAEkJJ6g",
+        name: "John Doe",
+        role: "Software Engineer"
+      },
+      content: [
+        {type: 'paragraph', content: 'Hey there 2 👋'},
+        {type: 'paragraph', content: 'Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate molestias sint numquam quidem, corporis, ipsa magni ducimus vel pariatur officia architecto maiores nostrum voluptas nisi non voluptates animi consectetur adipisci. 🚀'},
+        {type: 'link', content: 'lf.dev2/newproject'},
+      ],
+      publishedAt: new Date('2023-12-08 11:00:00')
+    },
+];
 
 export function App() {
   return (
@@ -16,8 +50,15 @@ export function App() {
 
       <Sidebar />
       <main>
-        <Post/>
-        <Post/>
+        {posts.map(post => {
+          return (
+            <Post 
+              author={post.author}
+              content={post.content}
+              publishedAt={post.publishedAt}
+            />
+          )
+        })}
       </main>
 
     </div>
--- src/Components/Post.jsx
+++ src/Components/Post.jsx
@@ -1,31 +1,47 @@
+import { format, formatDistanceToNow } from 'date-fns'
+import ptBR from 'date-fns/locale/pt-BR'
+
 import { Avatar } from './Avatar'
 import { Comment } from './Comment'
 import styles from './Post.module.css'
 
-export function Post(){
+export function Post({ author, publishedAt, content }){
+
+    const publishedDateFormatted = format(publishedAt, "MMMM dd 'at' hh:mm a")
+
+    // const publishedDateFormatted = format(publishedAt, "MMMM dd 'at' hh:mm a", {
+    //     locale: ptBR,
+    // })
+
+    const publishedDateRelativeToNow = formatDistanceToNow(publishedAt, {
+        addSuffix: true
+    })
+
     return (
         <article className={styles.post}>
             <header>
                 <div className={styles.author}>
-                    <Avatar src="https://media.licdn.com/dms/image/D4D03AQHpQ13Iz9Ps0w/profile-displayphoto-shrink_800_800/0/1692132894255?e=1707350400&v=beta&t=mzgF0v59NcabLRk_aYK0-s9qhtB6GVpbfwSuIFaAljw" />
+                    <Avatar src= {author.avatarUrl} />
                     <div className={styles.authorInfo}>
-                        <strong>Lucio Araujo</strong>
-                        <span>Software Developer</span>
+                        <strong>{author.name}</strong>
+                        <span>{author.role}</span>
                     </div>
                 </div>
 
-                <time title='December 7, 2023 at 11:49 PM' dateTime='2023-12-07 11:49:30'>Published 1 hour ago </time>
+                <time title={publishedDateFormatted} dateTime={publishedAt.toISOString()}>
+                    {publishedDateRelativeToNow} 
+                </time>
+
             </header>
 
             <div className={styles.content}>
-                <p>Hey there 👋</p>
-                <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate molestias sint numquam quidem, corporis, ipsa magni ducimus vel pariatur officia architecto maiores nostrum voluptas nisi non voluptates animi consectetur adipisci. 🚀</p>
-                <p><a href="#"> lf.dev/newproject</a></p>
-                <p>
-                    <a href="#">#newproject</a>{' '}
-                    <a href="#">#nlw</a>{' '}
-                    <a href="#">#rocketseat</a>
-                </p>
+                {content.map( item => {
+                    if (item.type === 'paragraph') {
+                        return <p>{item.content}</p>
+                    } else if (item.type === 'link') {
+                        return <p><a href="#">{item.content}</a></p>
+                    }
+                })}
             </div>
 
             <form className={styles.commentForm}>
